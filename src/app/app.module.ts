69652afdc1dfc7862b3c0c130fb3561c37c7f51d
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-04-06 07:54:10 +0300

    created a votes component to help with the upvotes and downvotes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { VotesComponent } from './votes/votes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    VotesComponent
   ],
   imports: [
     BrowserModule,
