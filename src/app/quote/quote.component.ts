a8b88ee2b2e2b75a00a597bad42054cf2b06597b
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-04-08 16:34:39 +0300

    Commit

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -19,7 +19,7 @@ export class QuoteComponent implements OnInit {
     new Quote('Mindfulness Daily','Sometimes we are tested, not to show our weakness, but to discover our strengths','F.R. Rishabh.S',68,0),
     new Quote('Foodliy.com','Take responsibility of your own happiness, never put it in other peoples hands','Roy T. Bennet',300,49),
     new Quote('Johnston Community School','The only person you are destined to be is the person you decide to be','Ralph Waldo Emerson',500,34),
-    new Quote('Winnie N.M','Only your best is good enough','Nyawira',1500,2)
+    new Quote('Winnie N.M','Only your best is good enough','Nyawira',1500,20)
 
   ]
   upvotes: number;
