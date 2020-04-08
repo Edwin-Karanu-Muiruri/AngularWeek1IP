4af906b1ae1598360e9b32e773d648de833222a7
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-04-08 18:14:08 +0300

    Auto-generated commit

--- src/app/add-quote-form.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AddQuoteForm } from './add-quote-form';
-
-describe('AddQuoteForm', () => {
-  it('should create an instance', () => {
-    expect(new AddQuoteForm()).toBeTruthy();
-  });
-});
